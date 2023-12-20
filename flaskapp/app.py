9b83e8030f313e4d5415a84bac38a3d420109545
kamjgree <kamjgree@iu.edu>
2023-12-19 23:31:35 -0500

    DEC19 DONE FOR THE NIGHT

--- flaskapp/app.py
+++ flaskapp/app.py
@@ -6,3 +6,23 @@ app = Flask(__name__)
 def index():
     """function to render the index.html template"""
     return render_template("index.html")
+
+@app.route("/contact_us/")
+def contact():
+    """function to render the contact.html template"""
+    return render_template("contact.html")
+
+@app.route("/our_story/")
+def our_story():
+    """function to render the our_story.html template"""
+    return render_template("our_story.html")
+
+@app.route("/our_services/")
+def our_services():
+    """function to render the our_services.html template"""
+    return render_template("our_services.html")
+
+@app.route("/our_markets/")
+def our_markets():
+    """function to render the our_markets.html template"""
+    return render_template("our_markets.html")
